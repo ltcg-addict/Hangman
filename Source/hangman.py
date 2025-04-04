72ac4fee05e1b4497d12098ea7e8bce154ee4073
Viaan Banglani <viaan390@gmail.com>
2025-04-04 12:48:15 +0800

    Implemented full GUI functionality

--- Source/hangman.py
+++ Source/hangman.py
@@ -44,10 +44,10 @@ theme = "anything"#input("Would you like the word to be about anything, programm
 words = open(theme + ".txt", "r").readlines()
 word = random.choice(words).lower().strip()
 live = -1
-lives = ["◯", "◯\n|", " ◯\n/|", " ◯\n/|arm", " ◯\n/|arm\n/", " ◯\n/|arm\n/ leg"]
 underscores = list("_" * len(word))
 
-underscores_text = font.render("".join(underscores), True, (255, 255, 255))
+window.fill((255, 255, 255))
+underscores_text = font.render("".join(underscores), True, (0, 0, 0))
 window.blit(underscores_text, (250 - (underscores_text.get_rect().width / 2), 400))
 pygame.display.flip()
 pygame.display.update()
@@ -62,35 +62,34 @@ while True:
       try:
         guess = char_eventkey_reference[event.key]
       except KeyError:
-        print("Not a letter")
         guess = " "
 
       if guess in word:
         while guess in word:
           underscores[word.find(guess)] = guess
-          underscores_text = font.render("".join(underscores), True, (255, 255, 255))
+          underscores_text = font.render("".join(underscores), True, (0, 0, 0))
 
           word = word.replace(guess, "_", 1)
-        print("".join(underscores))
         if word == "_" * len(word):
-          print("You win! The word was " + "".join(underscores))
           sys.exit(0)
       else:
         window.fill((255, 0, 0))
         time.sleep(0.75)
         pygame.display.flip()
         live += 1
-        print(lives[live])
+        lives_image = pygame.image.load(str(live) + ".png")
+        lives_image = pygame.transform.scale(lives_image, (300, 300))
 
-        if lives[live] == " ◯\n/|arm\n/ leg":
-          print("You lose. The word was " + p_word + " and the closest you got was " + "".join(underscores))
+        if live == 7:
+          time.sleep(1.25)
           sys.exit(0)
 
-      window.fill((0, 0, 0))
+      window.fill((255, 255, 255))
       window.blit(underscores_text, (250 - (underscores_text.get_rect().width / 2), 400))
+      window.blit(lives_image, (250 - (lives_image.get_rect().width / 2), 100))
 
-      pygame.display.update()
-      pygame.display.update()
+    pygame.display.update()
+    pygame.display.update()
 
 """while True:
   guess = input("Guess a letter ")
@@ -98,14 +97,14 @@ while True:
     while guess in word:
       underscores[word.find(guess)] = guess
       word = word.replace(guess, "_", 1)
-    print("".join(underscores))
+    ("".join(underscores))
     if word == "_" * len(word):
-      print("You win! The word was " + "".join(underscores))
+      ("You win! The word was " + "".join(underscores))
       sys.exit(0)
   else:
-    print("Not in word")
+    ("Not in word")
     live += 1
-    print(lives[live])
+    (lives[live])
     if lives[live] == "◯\n/|arm\n/ leg":
-      print("You lose. The word was " + p_word)
+      ("You lose. The word was " + p_word)
       sys.exit(0)"""
